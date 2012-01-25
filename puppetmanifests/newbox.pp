0a99207210049b3c6abd60ffdb33b87a194cb3eb
James Carr <james.r.carr@gmail.com>
2012-01-25 09:40:03 -0600

    removed puppet-rvm submodule

--- puppetmanifests/newbox.pp
+++ puppetmanifests/newbox.pp
@@ -1,16 +1,20 @@
+import 'puppet-rvm'
 class lucid32 {
   include mongodb
+  include rvm::system
+
+
   group { 'puppet': ensure => 'present' }
   exec { 'apt-get update':
     command => '/usr/bin/apt-get update'
   }
 
-  package { "apache2":
+  package { ["apache2", "php5", "php5-dev"]:
     ensure    => present,
     require => Exec['apt-get update']
   }
 
-  package { "vim":
+  package { ["vim", "zsh","mercurial"]:
     ensure    => present,
     require => Exec['apt-get update']
   }
@@ -31,6 +35,16 @@ class lucid32 {
     target => '/vagrant',
     force => true,
     require => Package['apache2']
+  }
+
+  file { '/etc/motd':
+     content => "Welcome to Arakis"
+
+  }
+  rvm_system_ruby {
+    'ruby-1.9.2-p290':
+      ensure => 'present',
+      default_use => true;
   }
 }
 
